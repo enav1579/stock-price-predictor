d782e5f8149d8580d4eaca4a78100d2222826f23
CE Navarro <navarro1579@gmail.com>
2025-06-14 19:46:36 -0600

    Simplify implementation using yfinance

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import requests
+import yfinance as yf
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
 import plotly.express as px
